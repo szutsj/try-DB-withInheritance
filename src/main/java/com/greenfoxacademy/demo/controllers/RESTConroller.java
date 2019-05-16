40fc44489414d7968a05f428f7d3ce1a8c34963f
Szuts Judit <judit.szuts.mob@gmail.com>
2019-05-16 12:59:13 +0200

    trying swagger

--- src/main/java/com/greenfoxacademy/demo/controllers/RESTConroller.java
+++ src/main/java/com/greenfoxacademy/demo/controllers/RESTConroller.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping
 public class RESTConroller {
   private BuildingService buildingService;
   private KingdomService kingdomService;
